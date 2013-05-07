54c7e6cf8acdc430e0c441d167e310a2dcbb4c95
Xmanliujun <644240319@qq.com>
2013-05-07 13:19:22 +0800

    Initial Commit

---
+++ HowMacthView/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  HowMacthView
+//
+//  Created by XmL on 13-5-7.
+//  Copyright (c) 2013年 XmL. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
